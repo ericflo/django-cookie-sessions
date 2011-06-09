bc3e6b6834a86b2b5d60469b5cf43b6424e8c71e
Eric Florenzano <floguy@gmail.com>
2011-06-09 12:32:36 +0200

    Make sure that exists() takes the session_key argument

--- cookiesessions/engine.py
+++ cookiesessions/engine.py
@@ -34,7 +34,7 @@ class SessionStore(SessionBase):
         self._session_key = self._get_session_key()
         self.modified = True
     
-    def exists(self):
+    def exists(self, session_key=None):
         """
         This method makes sense when you're talking to a shared resource, but
         it doesn't matter when you're storing the information in the client's
